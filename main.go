341e967d2e0b7d1171318cbb921acbad158db21f
ComputerScientist-01 <rathi406@gmail.com>
2022-01-16 16:25:10 +0530

    ✨ create router f(x)

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
+
+	router.GET(
+		"/ping",
+		func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message":"pong",
+			})
+		},
+	)
+
+	log.Println("starting server on port 9030")
 
 	router.Run(":9030")
 }
