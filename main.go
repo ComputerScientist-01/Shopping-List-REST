1d3cee996113633267d6ec2b17203f01ccfded3a
ComputerScientist-01 <rathi406@gmail.com>
2022-01-16 16:48:55 +0530

    ✨ create POST request

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type ShoppingList struct {
+	Name string `json:"name"`
+}
+
 func main() {
+
+	shoppingLists := make([]ShoppingList, 0)
+
 	router := gin.New()
 
 	router.GET(
@@ -15,6 +22,24 @@ func main() {
 		func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"message":"pong",
+			})
+		},
+	)
+
+	router.POST(
+		"/shopping-list/add",
+		func(c *gin.Context) {
+			var sl ShoppingList
+			if err := c.BindJSON(&sl); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"message": "bad request",
+				})
+				return
+			}
+
+			shoppingLists = append(shoppingLists, sl)
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Added shopping list",
 			})
 		},
 	)
